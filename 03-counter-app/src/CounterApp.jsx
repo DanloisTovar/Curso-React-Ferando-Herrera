41fef83f062af0c0e2a4731ea2a76f7706a6b7a3
Danlois Tovar <danlois.tovar@gmail.com>
2023-11-29 18:55:08 -0300

    practicas react eventos!

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
 
+// functions:
+const handleOnclick = (e) => {
+    console.log(e);
+};
 export const CounterApp = ({ value }) => {
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+
+            <button onClick={handleOnclick}>+1</button>
         </>
     );
 };
