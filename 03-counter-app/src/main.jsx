e4a826265a64239669903abf9414a1c7ba9059e4
Danlois Tovar <danlois.tovar@gmail.com>
2023-11-28 21:45:04 -0300

    primer componente!.

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const App = () => {
-    return <div>Hola Mundo!!!</div>;
-};
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <FirstApp />
     </React.StrictMode>,
 );
