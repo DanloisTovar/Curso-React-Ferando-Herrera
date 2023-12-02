1802bca178c17f42b00fb164a9d62aa4a5e3a6cd
Danlois Tovar <danlois.tovar@gmail.com>
2023-12-01 21:26:53 -0300

    practicas react testing!

--- 03-counter-app/test/CounterApp.test.jsx
+++ 03-counter-app/test/CounterApp.test.jsx
@@ -3,32 +3,29 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../src/CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
-    test('Debe arrancar el test', () => {
-        console.log('Arranque de test!');
-    });
     test('debe mostrar <CounterApp /> correctamente con el valor por defecto', () => {
-        const { getByText } = render(<CounterApp value={0} />);
-        expect(getByText('0')).toBeTruthy();
+        render(<CounterApp value={0} />);
+        expect(screen.getByText('0')).toBeTruthy();
     });
 
     test('Incrementar en "+1" con el boton click', () => {
-        const { getByText } = render(<CounterApp />);
-        fireEvent.click(getByText('+1'));
-        expect(getByText('1')).toBeTruthy();
+        render(<CounterApp />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('1')).toBeTruthy();
     });
 
     test('derecrementar en "-1" con el boton click', () => {
-        const { getByText } = render(<CounterApp />);
-        fireEvent.click(getByText('+1'));
-        fireEvent.click(getByText('-1'));
-        expect(getByText('0')).toBeTruthy();
+        render(<CounterApp />);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText('0')).toBeTruthy();
     });
 
     test('resets counter to initial value on "Reset" button click', () => {
-        const { getByText } = render(<CounterApp />);
-        fireEvent.click(getByText('+1'));
-        fireEvent.click(getByText('Reset'));
-        expect(getByText('0')).toBeTruthy();
+        render(<CounterApp />);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('Reset'));
+        expect(screen.getByText('0')).toBeTruthy();
     });
 
     test('debería coincidir con la instantánea', () => {
